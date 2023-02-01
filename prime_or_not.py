ccc2a39e296f4cd69aad6d68bdcc1c614dab1098
Manasa 1828 <gandamallamanasa2003@gmail.com>
2023-02-01 19:01:10 +0530

    Movie_Ticket_Booking_Application_Project

---
+++ prime_or_not.py
@@ -0,0 +1,13 @@
+"""#program to find prime or not
+n=int(input("enter a number"))
+for i in range(2,n//2):
+for i in range(2,sqrt(n)+1):
+for i in range(1,n+1):
+for i in range(2,n+1):"""
+
+#logic for number series in  
+n=int(input())
+if n in even:
+    n=n/2
+else:
+    n=3n+1
